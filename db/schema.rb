17a90da774741e7b5a955ce2b1bec270360f635f
game1095 <taweesak.in@gmail.com>
2017-06-09 17:09:34 +0700

    add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604141411) do
+ActiveRecord::Schema.define(version: 20170609100836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170604141411) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "finishedDate"
+    t.string "status"
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
